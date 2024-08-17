24f3c871fc7e52e85a2f16e393ceb95360be3c7f
kanishkar10k <114131838+kanishkar10k@users.noreply.github.com>
2024-08-17 10:17:57 +0530

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,31 @@
+var N=12;
+function format(n){
+    N=n;
+}
+function updateClock() {
+    var currentTime = new Date();
+    var hour = currentTime.getHours();
+    var minute = currentTime.getMinutes();
+    var second = currentTime.getSeconds();
+    switch(N){
+        case 12:{
+    var ampm = (hour < 12) ? "AM" : "PM";
+    hour = (hour > 12) ? hour - 12 : hour;
+    hour = (hour === 0) ? 12 : hour;
+
+    document.getElementById("hour").innerHTML = (hour < 10 ? "0" : "") + hour;
+    document.getElementById("minute").innerHTML = (minute < 10 ? "0" : "") + minute;
+    document.getElementById("second").innerHTML = (second < 10 ? "0" : "") + second;
+    document.getElementById("ampm").innerHTML = ampm;
+    }
+    case 24:{
+    hour=(hour >= 24)? 0 : hour;
+    document.getElementById("hour").innerHTML = (hour < 10 ? "0" : "") + hour;
+    document.getElementById("minute").innerHTML = (minute < 10 ? "0" : "") + minute;
+    document.getElementById("second").innerHTML = (second < 10 ? "0" : "") + second;
+    document.getElementById("ampm").innerHTML = "";
+    }
+}
+}
+
+setInterval(updateClock, 1000);
